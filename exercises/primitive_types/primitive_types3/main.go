ac534df8fa2a925d84557d347831c8a6d4804b2e
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-06-18 23:16:27 +0300

    Solved 'primitive_types' exercises

--- exercises/primitive_types/primitive_types3/main.go
+++ exercises/primitive_types/primitive_types3/main.go
@@ -1,12 +1,13 @@
 // primitive_types3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
+  who := "Paul";
+  country := "Ukraine";
 
 	fmt.Printf("Hello, I am %s and live in %s\n", who, country)
 }
